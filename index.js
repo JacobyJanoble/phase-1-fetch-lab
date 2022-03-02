66ebc01dfb5542217f1595b6334131e74c33463e
Jacob <Jacob.McCann214@gmail.com>
2022-03-02 16:10:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+const booksUrl = 'https://anapioficeandfire.com/api/books'
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch(booksUrl)
+  .then((resp) => resp.json())
+  .then((titlesData) => renderBooks(titlesData.message))
+};
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book;
     main.appendChild(h2);
   });
-}
+
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
